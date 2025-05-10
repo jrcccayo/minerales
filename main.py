7e929318f294d3d1449bb6970eef317295da753a
jrcccayo <cayoresourcesag@gmail.com>
2025-05-10 13:33:13 -0400

    main.py

--- main.py
+++ main.py
@@ -22706,6 +22706,7 @@ app = AppComplejos()
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8000"))  # Usa 8000 como puerto por defecto
     uvicorn.run(app, host="0.0.0.0", port=port)
-    
+
+app = ft.app(main, view=None)
 asgi_app = app
 
